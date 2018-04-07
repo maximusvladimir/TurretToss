9acf82adad823514efa7afe94d0cea6aea62fbc4
Max K <pyram3d@gmail.com>
2018-04-07 17:50:15 -0500

    Updated IP address.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //
 // Some initialization stuff. MUST come before requires:
 //
-global.websocketURL = "ws://34.210.98.26";
+global.websocketURL = "ws://34.215.198.119";
 global.websocketPort = 3000;
 if (process.env.DEBUG == "WCYK:* ") {
 	global.websocketURL = "ws://localhost";
